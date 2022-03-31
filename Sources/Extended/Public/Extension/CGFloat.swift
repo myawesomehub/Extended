e8c4d053848a53475d486a0e8f641606d138e652
Mohammad Yasir <mdcode2021@gmail.com>
2022-03-31 10:22:28 +0530

    Added CGFloat extension

---
+++ Sources/Extended/Public/Extension/CGFloat.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohammad Yasir on 31/03/22.
+//
+
+import SwiftUI
+
+extension CGFloat {
+    static var screenWidth: CGFloat { UIScreen.main.bounds.width }
+}
+
+extension CGFloat {
+    static var screenHeight: CGFloat { UIScreen.main.bounds.height }
+}

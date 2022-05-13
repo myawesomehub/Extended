f901d1bff5298141580df22ba1834e7215a382e7
Gregory Boyd <gboyd69@yahoo.com>
2022-05-12 21:24:25 -0400

    Added a  static function that calculates the difference between two dates.  Returns and integer that represents the difference between two dates entered by the user.

--- Sources/Extended/Public/Extension/Int.swift
+++ Sources/Extended/Public/Extension/Int.swift
@@ -10,3 +10,8 @@ import SwiftUI
 public extension Int {
     var squared: Int { self * self }
 }
+
+static func diffOfDays(from: Date, to: Date) -> Int {
+    let numOfDays = Calendar.current.dateComponents([.day], from: from, to: to)
+    return numOfDays.day!
+}
